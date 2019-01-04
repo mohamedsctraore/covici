223bacfbda770dbfed98d3e29e7f1a8be6762600
Mohamed Traore <mohamed.s.c.traore@gmail.com>
2019-01-04 15:46:21 +0000

    Initialisation du projet

---
+++ src/main/java/org/covici/website/WebsiteCoviciApplication.java
@@ -0,0 +1,14 @@
+package org.covici.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteCoviciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteCoviciApplication.class, args);
+	}
+
+}
+
